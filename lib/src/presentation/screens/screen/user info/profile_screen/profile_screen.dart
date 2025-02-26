7f2d87941c9ef54c2a37131c8175e53ca0a79296
Ahmed-Hany-saad <ahhasa842@gmail.com>
2025-02-26 17:20:21 +0200

    update profile_screen.dart page

--- lib/src/presentation/screens/screen/user info/profile_screen/profile_screen.dart
+++ lib/src/presentation/screens/screen/user info/profile_screen/profile_screen.dart
@@ -19,47 +19,40 @@ import 'package:short_path/src/presentation/shared_widgets/custom_auth_button.da
 import 'package:short_path/src/presentation/shared_widgets/custom_auth_text_feild.dart';
 import 'package:short_path/src/short_path.dart';
 
+import '../../../../shared_widgets/progress_bar.dart';
+
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     ProfileViewmodel viewModel = getIt.get<ProfileViewmodel>();
+
     return BlocProvider(
       create: (context) => viewModel,
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Personal Details',
-          ),
-        ),
         body: SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 30.h),
             child: BlocConsumer<ProfileViewmodel, ProfileState>(
               listener: (context, state) {
-                switch (state) {
-                  case ProfileLoading():
-                    showLoading(context, 'Adding profile...');
-                  case ProfileUpdateSuccess():
-                    navKey.currentState!.pushNamedAndRemoveUntil(
-                        RoutesName.language, (route) => false);
-                  case ProfileUpdateError():
-                    showAwesomeDialog(context,
-                        title: 'error',
-                        desc: state.message,
-                        onOk: () {},
-                        dialogType: DialogType.error);
-                  default:
+                if (state is ProfileLoading) {
+                  showLoading(context, 'Adding profile...');
+                } else if (state is ProfileUpdateSuccess) {
+                  navKey.currentState!.pushNamedAndRemoveUntil(
+                      RoutesName.language, (route) => false);
+                } else if (state is ProfileUpdateError) {
+                  showAwesomeDialog(context,
+                      title: 'Error',
+                      desc: state.message,
+                      onOk: () {},
+                      dialogType: DialogType.error);
                 }
               },
-              buildWhen: (previous, current) {
-                return current is ProfileInitialState ||
-                    current is ValidateColorButtonState;
-              },
+              buildWhen: (previous, current) =>
+              current is ProfileInitialState || current is ValidateColorButtonState,
               listenWhen: (previous, current) {
-                if (previous is ProfileLoading ||
-                    current is ProfileUpdateError) {
+                if (previous is ProfileLoading || current is ProfileUpdateError) {
                   hideLoading();
                 }
                 return current is! ProfileInitialState;
@@ -67,74 +60,66 @@ class ProfileScreen extends StatelessWidget {
               builder: (context, state) {
                 final viewModel = context.read<ProfileViewmodel>();
 
-                if (state is ValidateColorButtonState ||
-                    state is ProfileInitialState) {
-                  return Form(
-                    key: viewModel.formKey,
-                    onChanged: viewModel.validateColorButton,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Center(child: HeaderWidget()),
-                        verticalSpace(30),
-                        JobTitleInput(viewModel: viewModel),
-                        verticalSpace(20),
-                        PortfolioInput(viewModel: viewModel),
-                        if (viewModel.portfolioLinks.isNotEmpty)
-                          const PortfolioListWidget(),
-                        verticalSpace(20),
-                        CustomTextFormField(
-                          hintText: 'Enter your LinkedIn profile URL',
-                          keyboardType: TextInputType.url,
-                          controller: viewModel.linkedInController,
-                          labelText: 'LinkedIn Profile',
-                          validator: (value) {
-                            return validateLink(value);
-                          },
+                if (state is ValidateColorButtonState || state is ProfileInitialState) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      verticalSpace(40),
+                      Center(child: HeaderWidget()),
+                      Center(
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 20.h),
+                          child: StepProgressBar(currentStep: 1), // Pass only current step
                         ),
-                        verticalSpace(20),
-                        CustomTextFormField(
-                          hintText: 'Enter your Profile Picture URL',
-                          keyboardType: TextInputType.url,
-                          controller: viewModel.profilePictureController,
-                          labelText: 'Profile Picture',
-                          validator: (value) {
-                            return validateLink(value);
-                          },
+                      ),
+                      verticalSpace(30),
+                      Form(
+                        key: viewModel.formKey,
+                        onChanged: viewModel.validateColorButton,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            JobTitleInput(viewModel: viewModel),
+                            verticalSpace(20),
+                            PortfolioInput(viewModel: viewModel),
+                            if (viewModel.portfolioLinks.isNotEmpty)
+                              const PortfolioListWidget(),
+                            verticalSpace(20),
+                            CustomTextFormField(
+                              hintText: 'Enter your LinkedIn profile URL',
+                              keyboardType: TextInputType.url,
+                              controller: viewModel.linkedInController,
+                              labelText: 'LinkedIn Profile',
+                              validator: validateLink,
+                            ),
+                            verticalSpace(20),
+                            CustomTextFormField(
+                              hintText: 'Enter your Profile Picture URL',
+                              keyboardType: TextInputType.url,
+                              controller: viewModel.profilePictureController,
+                              labelText: 'Profile Picture',
+                              validator: validateLink,
+                            ),
+                            verticalSpace(20),
+                            CustomTextFormField(
+                              hintText: 'Enter your Bio',
+                              keyboardType: TextInputType.text,
+                              controller: viewModel.bioController,
+                              labelText: 'Summary',
+                              validator: validateSummary,
+                            ),
+                            verticalSpace(30),
+                            CustomAuthButton(
+                              text: 'NEXT',
+                              onPressed: viewModel.next,
+                              color: viewModel.validate
+                                  ? AppColors.primaryColor
+                                  : const Color(0xFF5C6673),
+                            ),
+                          ],
                         ),
-                        verticalSpace(20),
-                        CustomTextFormField(
-                          hintText: 'Enter your Bio',
-                          keyboardType: TextInputType.url,
-                          controller: viewModel.bioController,
-                          labelText: 'Summary',
-                          validator: (value) {
-                            return validateSummary(value);
-                          },
-                        ),
-                        verticalSpace(20),
-                        // LanguageInput(viewModel: viewModel),
-                        // verticalSpace(20),
-                        // if (viewModel.filteredLanguageSuggestions.isNotEmpty &&
-                        //     viewModel.languageController.text.isNotEmpty)
-                        //   SuggestionList(
-                        //     suggestions: languageSuggestions,
-                        //     onTap: (int index) {
-                        //       viewModel.selectLanguage(index);
-                        //     },
-                        //   ),
-                        // if (viewModel.languages.isNotEmpty)
-                        //   const LanguageListWidget(),
-                        // verticalSpace(30),
-                        CustomAuthButton(
-                          text: 'NEXT',
-                          onPressed: viewModel.next,
-                          color: viewModel.validate
-                              ? AppColors.primaryColor
-                              : const Color(0xFF5C6673),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   );
                 }
                 return const Center(child: CircularProgressIndicator());
--- lib/src/short_path.dart
+++ lib/src/short_path.dart
@@ -18,7 +18,7 @@ class ShortPath extends StatefulWidget {
 }
 
 class _ShortPathState extends State<ShortPath> {
-  String? _initialRoute = RoutesName.login;
+  String? _initialRoute = RoutesName.profile;
   bool _isInitialized = true;
 
   @override
