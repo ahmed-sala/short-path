53af3f3fc6f5829720f9e4dc92e2fdbc1ff3df87
Ahmed Salah Aldin Ahmed <92693085+ahmed-sala@users.noreply.github.com>
2025-04-13 13:44:33 +0200

    feat: Enhance UI directionality and improve localization in multiple screens

--- lib/src/presentation/screens/screen/user info/profile_screen/profile_screen.dart
+++ lib/src/presentation/screens/screen/user info/profile_screen/profile_screen.dart
@@ -33,112 +33,115 @@ class ProfileScreen extends StatelessWidget {
             'Personal Details',
           ),
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 30.h),
-            child: BlocConsumer<ProfileViewmodel, ProfileState>(
-              listener: (context, state) {
-                switch (state) {
-                  case ProfileLoading():
-                    showLoading(context, 'Adding profile...');
-                  case ProfileUpdateSuccess():
-                    navKey.currentState!.pushNamedAndRemoveUntil(
-                        RoutesName.language, (route) => false);
-                  case ProfileUpdateError():
-                    showAwesomeDialog(context,
-                        title: 'error',
-                        desc: state.message,
-                        onOk: () {},
-                        dialogType: DialogType.error);
-                  default:
-                }
-              },
-              buildWhen: (previous, current) {
-                return current is ProfileInitialState ||
-                    current is ValidateColorButtonState;
-              },
-              listenWhen: (previous, current) {
-                if (previous is ProfileLoading ||
-                    current is ProfileUpdateError) {
-                  hideLoading();
-                }
-                return current is! ProfileInitialState;
-              },
-              builder: (context, state) {
-                final viewModel = context.read<ProfileViewmodel>();
+        body: Directionality(
+          textDirection: TextDirection.ltr,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 30.h),
+              child: BlocConsumer<ProfileViewmodel, ProfileState>(
+                listener: (context, state) {
+                  switch (state) {
+                    case ProfileLoading():
+                      showLoading(context, 'Adding profile...');
+                    case ProfileUpdateSuccess():
+                      navKey.currentState!.pushNamedAndRemoveUntil(
+                          RoutesName.language, (route) => false);
+                    case ProfileUpdateError():
+                      showAwesomeDialog(context,
+                          title: 'error',
+                          desc: state.message,
+                          onOk: () {},
+                          dialogType: DialogType.error);
+                    default:
+                  }
+                },
+                buildWhen: (previous, current) {
+                  return current is ProfileInitialState ||
+                      current is ValidateColorButtonState;
+                },
+                listenWhen: (previous, current) {
+                  if (previous is ProfileLoading ||
+                      current is ProfileUpdateError) {
+                    hideLoading();
+                  }
+                  return current is! ProfileInitialState;
+                },
+                builder: (context, state) {
+                  final viewModel = context.read<ProfileViewmodel>();
 
-                if (state is ValidateColorButtonState ||
-                    state is ProfileInitialState) {
-                  return Form(
-                    key: viewModel.formKey,
-                    onChanged: viewModel.validateColorButton,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Center(child: HeaderWidget()),
-                        verticalSpace(30),
-                        JobTitleInput(viewModel: viewModel),
-                        verticalSpace(20),
-                        PortfolioInput(viewModel: viewModel),
-                        if (viewModel.portfolioLinks.isNotEmpty)
-                          const PortfolioListWidget(),
-                        verticalSpace(20),
-                        CustomTextFormField(
-                          hintText: 'Enter your LinkedIn profile URL',
-                          keyboardType: TextInputType.url,
-                          controller: viewModel.linkedInController,
-                          labelText: 'LinkedIn Profile',
-                          validator: (value) {
-                            return validateLink(value);
-                          },
-                        ),
-                        verticalSpace(20),
-                        CustomTextFormField(
-                          hintText: 'Enter your Profile Picture URL',
-                          keyboardType: TextInputType.url,
-                          controller: viewModel.profilePictureController,
-                          labelText: 'Profile Picture',
-                          validator: (value) {
-                            return validateLink(value);
-                          },
-                        ),
-                        verticalSpace(20),
-                        CustomTextFormField(
-                          hintText: 'Enter your Bio',
-                          keyboardType: TextInputType.url,
-                          controller: viewModel.bioController,
-                          labelText: 'Summary',
-                          validator: (value) {
-                            return validateSummary(value);
-                          },
-                        ),
-                        verticalSpace(20),
-                        // LanguageInput(viewModel: viewModel),
-                        // verticalSpace(20),
-                        // if (viewModel.filteredLanguageSuggestions.isNotEmpty &&
-                        //     viewModel.languageController.text.isNotEmpty)
-                        //   SuggestionList(
-                        //     suggestions: languageSuggestions,
-                        //     onTap: (int index) {
-                        //       viewModel.selectLanguage(index);
-                        //     },
-                        //   ),
-                        // if (viewModel.languages.isNotEmpty)
-                        //   const LanguageListWidget(),
-                        // verticalSpace(30),
-                        CustomAuthButton(
-                          text: 'NEXT',
-                          onPressed: viewModel.next,
-                          color: viewModel.validate
-                              ? AppColors.primaryColor
-                              : const Color(0xFF5C6673),
-                        ),
-                      ],
-                    ),
-                  );
-                }
-                return const Center(child: CircularProgressIndicator());
-              },
+                  if (state is ValidateColorButtonState ||
+                      state is ProfileInitialState) {
+                    return Form(
+                      key: viewModel.formKey,
+                      onChanged: viewModel.validateColorButton,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Center(child: HeaderWidget()),
+                          verticalSpace(30),
+                          JobTitleInput(viewModel: viewModel),
+                          verticalSpace(20),
+                          PortfolioInput(viewModel: viewModel),
+                          if (viewModel.portfolioLinks.isNotEmpty)
+                            const PortfolioListWidget(),
+                          verticalSpace(20),
+                          CustomTextFormField(
+                            hintText: 'Enter your LinkedIn profile URL',
+                            keyboardType: TextInputType.url,
+                            controller: viewModel.linkedInController,
+                            labelText: 'LinkedIn Profile',
+                            validator: (value) {
+                              return validateLink(value);
+                            },
+                          ),
+                          verticalSpace(20),
+                          CustomTextFormField(
+                            hintText: 'Enter your Profile Picture URL',
+                            keyboardType: TextInputType.url,
+                            controller: viewModel.profilePictureController,
+                            labelText: 'Profile Picture',
+                            validator: (value) {
+                              return validateLink(value);
+                            },
+                          ),
+                          verticalSpace(20),
+                          CustomTextFormField(
+                            hintText: 'Enter your Bio',
+                            keyboardType: TextInputType.url,
+                            controller: viewModel.bioController,
+                            labelText: 'Summary',
+                            validator: (value) {
+                              return validateSummary(value);
+                            },
+                          ),
+                          verticalSpace(20),
+                          // LanguageInput(viewModel: viewModel),
+                          // verticalSpace(20),
+                          // if (viewModel.filteredLanguageSuggestions.isNotEmpty &&
+                          //     viewModel.languageController.text.isNotEmpty)
+                          //   SuggestionList(
+                          //     suggestions: languageSuggestions,
+                          //     onTap: (int index) {
+                          //       viewModel.selectLanguage(index);
+                          //     },
+                          //   ),
+                          // if (viewModel.languages.isNotEmpty)
+                          //   const LanguageListWidget(),
+                          // verticalSpace(30),
+                          CustomAuthButton(
+                            text: 'NEXT',
+                            onPressed: viewModel.next,
+                            color: viewModel.validate
+                                ? AppColors.primaryColor
+                                : const Color(0xFF5C6673),
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+                  return const Center(child: CircularProgressIndicator());
+                },
+              ),
             ),
           ),
         ),
